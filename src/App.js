f0779c033a780bebf7ffdd2236ca0bfc9b92c18b
やまだ <ken17yama@gmail.com>
2022-06-17 23:07:09 +0900

    konvaで画像を表示

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./styles.css";
+import { Stage, Layer, Image } from "react-konva";
+import useImage from "use-image";
+
+const LionImage = () => {
+  const [image] = useImage("https://konvajs.org/assets/lion.png");
+  return <Image image={image} />;
+};
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-    </div>
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <LionImage />
+      </Layer>
+    </Stage>
   );
 }
